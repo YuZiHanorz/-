8d806f8d07701277ab44d73bb57f5cd5b9895d73
laonahongchen <chenweizhe1999@126.com>
2018-06-04 22:39:10 +0800

    fahuiud

--- command.cpp
+++ command.cpp
@@ -38,9 +38,9 @@ void init() {
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	//ios::sync_with_stdio(false);
+	//cin.tie(0);
+	//cout.tie(0);
 	init();
 	bplus_tree<ticket> theticket("ticket.db");
 	bplus_tree<train> thetrain("train.db");
